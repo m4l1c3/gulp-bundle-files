0f3ee1b79ad40720ba3079980b916deda006d9d3
Jon Wisdom <jon@jonsomnia.com>
2016-02-15 21:17:03 -0700

    Adding bithoundrc, eslintrc, tweaking a few things in task factory and bundle checks to correct linting issues.

--- bundleCheck.js
+++ bundleCheck.js
@@ -2,33 +2,43 @@
 var fs = require('fs'),
 	path = require('path');
 
+var currentBundle = function(i, items) {
+	var self = this;
+	self.index = i;
+	self.name = items[self.index];
+
+	self.isLast = function() {
+		var self = this;
+		self.index = i;
+		self.name = items[self.index];
+
+		self.isLast = function() {
+			if ((parseInt(self.index) !== items.length - 1) && (self.index !== self.name)) {
+				return false;
+			}
+			return true;
+		};
+
+		self.getName = function() {
+			return self.name;
+		};
+
+		return self;
+	};
+
+	self.getName = function() {
+		return self.name;
+	};
+
+	return self;
+};
+
 var bundleCheck = function(bundles) {
 	var missing_bundle_items = [];
 	var items = bundles;
 	for(var i in items) {
 		if (!fs.existsSync(path.join(__dirname, items[i]))) {
-			var currentBundle = function(i) {
-				var self = this;
-				self.index = i;
-				self.name = items[self.index];
-
-				self.isLast = function() {
-					if (parseInt(self.index) === items.length - 1) {
-						return true;
-					} else if (self.index === self.name) {
-						return true;
-					} else {
-						return false;
-					}
-				};
-
-				self.getName = function() {
-					return self.name;
-				};
-
-				return self;
-			};
-			missing_bundle_items.push(new currentBundle(i));
+			missing_bundle_items.push(new currentBundle(i, items));
 		}
 	}
 	if(!missing_bundle_items.length) {
--- gulpTaskFactory.js
+++ gulpTaskFactory.js
@@ -15,41 +15,44 @@ exports.gulpTaskFactory = function(gulpTask, options) {
 		var isJs = gulpTask.endsWith('.js'),
 			isCss = !isJs;
 
-		if (isJs) {
-			try {
-				return gulp.src(options.files[gulpTask])
-					.pipe(
-						gulpif(
-							options.concat !== undefined && options.concat.active && isJs,
-							concat(gulpTask)
-						)
+		try {
+			return gulp.src(options.files[gulpTask])
+				.pipe(
+					gulpif(
+						options.concat !== undefined && options.concat.active && isJs,
+						function() {
+							return concat(gulpTask);
+						}
 					)
-					.pipe(
-						gulpif(
-							options.uglify !== undefined && options.uglify.active && isJs,
-							uglify(options.uglify.config)
-						)
+				)
+				.pipe(
+					gulpif(
+						options.uglify !== undefined && options.uglify.active && isJs,
+						function() {
+							return uglify(options.uglify.config);
+						}
 					)
-					.pipe(gulp.dest(path.join(options.destinationFolder, 'js')));
-			} catch (ex) {
-				gutil.log(gutil.colors.red('Error creating Javascript bundle: ' + gulpTask));
-				throw new PluginError(config.name, 'Error creating Javascript bundle.');
-			}
-		}
-		else if (isCss) {
-			try {
-				return gulp.src(options.files[gulpTask])
-					.pipe(
-						gulpif(
-							options.less !== undefined && options.less.active && isCss,
-							less(options.less.config)
-						)
+				)
+				.pipe(
+					gulpif(
+						options.less !== undefined && options.less.active && isCss,
+						function() {
+							return less(options.less.config);
+						}
 					)
-					.pipe(gulp.dest(path.join(options.destinationFolder, 'css')));
-			} catch (ex) {
-				gutil.log(gutil.colors.red('Error creating CSS bundle: ' + gulpTask));
-				throw new PluginError(config.name, 'Error creating CSS bundle.');
-			}
+				)
+				.pipe(
+					gulpif(
+						options.autoprefixer !== undefined && options.autoprefixer.active && isCss,
+						function() {
+							return autoprefixer(options.autoprefixer.config);
+						}
+					)
+				)
+				.pipe(gulp.dest(path.join(options.destinationFolder, 'js')));
+		} catch (ex) {
+			gutil.log(gutil.colors.red('Error creating bundle: ' + gulpTask));
+			throw new PluginError(config.name, 'Error creating bundle.');
 		}
 	});
 };
