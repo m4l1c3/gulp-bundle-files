481bd7d430e44a5ef37cfa37b0fe24e0c220606b
m4l1c3 <jon@jonsomnia.com>
2016-02-19 15:17:27 -0700

    Adding gulpfile and sample config for testing.

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp'),
+    gulpBundleFiles = require('gulp-bundle-files'),
+    bundles = require('options.json');
+
+gulp.task('bundle', function() {
+    return gulp.src()
+});
+
+gulp.task('default', ['bundle']);
